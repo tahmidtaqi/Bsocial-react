0cc385be5cfda9a97088c0a2b9c339f1077c88be
tahmidtaqi <taqitahmid97@gmail.com>
2020-12-21 01:57:44 +0600

    sign up modal

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import login_background from '../../images/background/2.jpg'
 import Header from '../Header/Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
+    
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
     return (
         <div>
             <div className="container-fluid login-page">
@@ -25,7 +35,7 @@ const Login = () => {
                                 <br></br>
                                 <button className="login-button"> Log into your account</button>
                             </form>
-                            <p className="signup-paragraph mt-2">Already have an account? <a href="">Signup</a></p>
+                            <p className="signup-paragraph mt-2">Already have an account?  <Button variant="primary" onClick={handleShow}>Signup</Button></p>
                         </div>
                         <div className="container login-page-lower-area">
                             <button className="semi-transparent-button">About Us</button>
@@ -35,6 +45,39 @@ const Login = () => {
                     </div>
                 </div>
             </div>
+            <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>SIgn Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><Form>
+    <Form.Group controlId="formGroupName">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter Name" />
+  </Form.Group>
+  <Form.Group controlId="formGroupEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+  <Form.Group controlId="formGroupPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Button type="submit" variant="primary" onClick={handleClose}>
+   Sign Up
+</Button>
+</Form>
+</Modal.Body>
+{/*
+<Modal.Footer>
+<Button variant="secondary" onClick={handleClose}>
+    Close
+</Button>
+<Button variant="primary" onClick={handleClose}>
+    Save Changes
+</Button>
+</Modal.Footer>
+*/}
+</Modal>
         </div>
     );
 };
